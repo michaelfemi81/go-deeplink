07c259913241bae14f74b6fcfdf9b210ac2e669c
Michael Odekunle <michaelfemi81@gmail.com>
2016-05-18 16:42:52 +0100

    first commit

---
+++ deeplink.go
@@ -0,0 +1,99 @@
+package deeplink
+
+import (
+	"fmt"
+	"github.com/michaelfemi81/go-inliner"
+
+	"os"
+	"strings"
+	"net/http"
+	"html/template"
+
+	//"reflect"
+	"errors"
+)
+type errorString struct {
+	s string
+}
+
+func (e *errorString) Error() string {
+	return e.s
+}
+type DeepLink struct {
+	Android_package_name string
+	Title                string
+	Ios_store_link       string
+	Fallback             string
+	Url string
+}
+
+func (d *DeepLink) init(f string, t... string) (error) {
+	d.Fallback = f
+	if (strings.Trim(f, " ") == "") {
+		return errors.New("Invalid Fallback");
+	}
+	if (len(t) < 1) {
+		return errors.New("Set At Least Android Package");
+	} else {
+		if (len(t) == 1) {
+			d.Android_package_name = t[0]
+			d.Ios_store_link = ""
+			d.Title = ""
+		} else if (len(t) == 2) {
+			d.Android_package_name = t[0]
+			d.Ios_store_link = t[1]
+			d.Title = ""
+		} else {
+			d.Android_package_name = t[0]
+			d.Ios_store_link = t[1]
+			d.Title = t[2]
+		}
+	}
+
+
+
+
+return nil
+}
+func(d DeepLink)DoDeepLink(w http.ResponseWriter, r *http.Request)(err error){
+	url:=r.URL.Query()["url"];
+	//fmt.Println(d.Title)
+	if(url==nil){
+		err=errors.New("no query uri");
+		fmt.Fprintln(w,"Invalid Query String");
+		return
+	}else{
+
+		d.Url=url[0]
+
+		t := template.New("index.html")
+		t,err:=t.ParseFiles("./public/index.html");
+		fil, err := os.OpenFile("./public/aa.html", os.O_WRONLY|os.O_CREATE, 0666)
+		if err != nil {
+			fmt.Println(err)
+			//  os.Exit(1)
+		}
+		err=t.Execute(fil,d);
+		if err != nil {
+			fmt.Println(err)
+			//  os.Exit(1)
+		}else{
+			inliner.RenderToHttp("./public/aa.html",w,r);
+
+		}
+		defer remove("./public/aa.html")
+		defer fil.Close()
+
+
+
+}
+
+
+return
+}
+func remove(aa string){
+	err3 := os.Remove(aa)
+	if err3 != nil {
+		fmt.Println(err3)
+	}
+}
---
+++ public/script.js
@@ -0,0 +1,90 @@
+
+function deep_link (options) {
+  var fallback = options.fallback || ''
+  var url = options.url || ''
+  var ios_store_link = options.ios_store_link
+  var android_package_name = options.android_package_name
+  var play_store_link = 'https://market.android.com/details?id=' + android_package_name
+  var ua = window.navigator.userAgent
+
+  // split the first :// from the url string
+  var split = url.split(/:\/\/(.+)/)
+  var scheme = split[0]
+  var path = split[1]
+
+  var urls = {
+    deep_link: url,
+    ios_store_link: ios_store_link,
+    android_intent: 'intent://' + path + '#Intent;scheme=' + scheme + ';package=' + android_package_name + ';end;',
+    play_store_link: play_store_link,
+    fallback: fallback
+  }
+//console.log(urls);
+  var is_mobile = {
+    android: function () {
+      return /Android/i.test(ua)
+    },
+    ios: function () {
+      return /iPhone|iPad|iPod/i.test(ua)
+    }
+  }
+
+  // fallback to the application store on mobile devices
+  if (is_mobile.ios() && urls.deep_link && urls.ios_store_link) {
+    ios_launch()
+  } else if (is_mobile.android() && android_package_name) {
+    android_launch()
+  } else {
+    window.location = urls.fallback
+  }
+
+  function launch_webkit_approach (url, fallback) {
+    document.location = url
+    setTimeout(function () {
+      document.location = fallback
+    }, 250)
+  }
+
+  function launch_iframe_approach (url, fallback) {
+    var iframe = document.createElement('iframe')
+    iframe.style.border = 'none'
+    iframe.style.width = '1px'
+    iframe.style.height = '1px'
+    iframe.onload = function () {
+      document.location = url
+    }
+    iframe.src = url
+
+    window.onload = function () {
+      document.body.appendChild(iframe)
+
+      setTimeout(function () {
+        window.location = fallback
+      }, 25)
+    }
+  }
+
+  function ios_launch () {
+    // Chrome and Safari on IOS-9 don't allow the iframe approach
+    if (ua.match(/CriOS/) || (ua.match(/Safari/) && ua.match(/Version\/9/))) {
+      launch_webkit_approach(urls.deep_link, urls.ios_store_link || urls.fallback)
+    } else {
+      launch_iframe_approach(urls.deep_link, urls.ios_store_link || urls.fallback)
+    }
+  }
+
+  function android_launch () {
+    if (ua.match(/Chrome/)) {
+      document.location = urls.android_intent
+    } else if (ua.match(/Firefox/)) {
+      launch_webkit_approach(urls.deep_link, urls.play_store_link || urls.fallback)
+    } else {
+      launch_iframe_approach(url, urls.play_store_link || urls.fallback)
+    }
+  }
+}
+
+// expose module so it can be required later in tests
+if (typeof module !== 'undefined') {
+  module.exports = deep_link
+}
